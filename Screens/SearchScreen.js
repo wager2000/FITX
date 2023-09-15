5c2934dcc1135f038d8b18b77d78338952019dc5
wager2000 <32940039+wager2000@users.noreply.github.com>
2023-09-15 10:10:19 +0200

    på vej til markers

--- Screens/SearchScreen.js
+++ Screens/SearchScreen.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, TextInput } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import { collection, addDoc, getDocs } from "firebase/firestore"; 
+import { db } from '../firebaseConfig';
+
+
 
 const SearchScreen = () => {
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+
+    
+    // Fetch places from Firestore
+    const fetchPlaces = async () => {
+        const placesRef = collection(db, 'Places');
+        const querySnapshot = await getDocs(placesRef);
+      
+        const places = [];
+        querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          places.push(data);
+        });
+      
+        console.log('Fetched Places:', places);
+      };
+      
+      // Call the function to fetch places
+      fetchPlaces();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.searchContainer}>
@@ -13,47 +40,50 @@ const SearchScreen = () => {
         />
       </View>
       <MapView
-  style={styles.map}
-  initialRegion={{
-    latitude: 55.6761,
-    longitude: 12.5683,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  }}
->
-  {/* Add markers or other map components as needed */}
-  <Marker
-    coordinate={{
-      latitude: 55.6761,
-      longitude: 12.5683,
-    }}
-    title="Copenhagen"
-    description="Capital of Denmark"
-  />
-</MapView>
+        style={styles.map}
+        initialRegion={{
+          latitude: 55.6761,
+          longitude: 12.5683,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        {/* Add markers for each place */}
+        {places.map((place) => (
+          <Marker
+            key={place.id}
+            coordinate={{
+              latitude: place.Place.latitude,
+              longitude: place.Place.longitude,
+            }}
+            title={place.Name}
+            description={place.Description}
+          />
+        ))}
+      </MapView>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  searchContainer: {
-    padding: 16,
-    backgroundColor: '#FFFFFF',
-    borderBottomWidth: 1,
-    borderBottomColor: '#DDDDDD',
-  },
-  searchInput: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    paddingHorizontal: 8,
-  },
-  map: {
-    flex: 1,
-  },
-});
-
-export default SearchScreen;
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    searchContainer: {
+      padding: 16,
+      backgroundColor: '#FFFFFF',
+      borderBottomWidth: 1,
+      borderBottomColor: '#DDDDDD',
+    },
+    searchInput: {
+      height: 40,
+      borderColor: 'gray',
+      borderWidth: 1,
+      paddingHorizontal: 8,
+    },
+    map: {
+      flex: 1,
+    },
+  });
+  
+  export default SearchScreen;
