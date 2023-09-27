a1b92d20e42c4d8d8df46267717a89cec68db6bd
Viktor <viso21ad@student.cbs.dk>
2023-09-27 12:17:31 +0200

    Removal of login

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ import LoginScreen from "./Components/LoginScreen";
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
-/*export default function App() {
+export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
@@ -55,62 +55,6 @@ const Stack = createStackNavigator();
         <Tab.Screen name="Details" component={StackNavigatorDetails} />
       </Tab.Navigator>
     </NavigationContainer>
-  );
-}*/
-
-//with login
-export default function App() {
-  const TabNavigator = () => {
-    return (
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-
-              if (route.name === "Start") {
-                iconName = focused ? "home" : "home-outline";
-              } else if (route.name === "Search") {
-                iconName = focused ? "search" : "search-outline";
-              } else if (route.name === "Settings") {
-                iconName = focused ? "settings" : "settings-outline";
-              } else if (route.name === "History") {
-                iconName = focused ? "time" : "time-outline";
-              } else if (route.name === "Details") {
-                iconName = focused
-                  ? "information-circle"
-                  : "information-circle-outline";
-              }
-
-              // You can customize the appearance of the icons here
-
-              return <Ionicons name={iconName} size={size} color={color} />;
-            },
-          })}
-          tabBarOptions={{
-            activeTintColor: "blue", // Color of the active tab
-            inactiveTintColor: "gray", // Color of inactive tabs
-            style: {
-              backgroundColor: "white", // Background color of the tab bar
-            },
-          }}
-        >
-          <Tab.Screen name="Start" component={Startscreen} />
-          <Tab.Screen name="Search" component={SearchScreen} />
-          <Tab.Screen name="History" component={HistoryScreen} />
-          <Tab.Screen name="Details" component={StackNavigatorDetails} />
-        </Tab.Navigator>
-    );
-  };
-  return (
-    <NavigationContainer>
-
-    <Stack.Navigator>
-      <Stack.Screen name={"RegistrationScreen"} component={RegistrationScreen} />
-      <Stack.Screen name={"LoginScreen"} component={LoginScreen} />
-      <Stack.Screen name={"TabNavigator"} component={TabNavigator} />
-    </Stack.Navigator>
-    </NavigationContainer>
-
   );
 }
 
